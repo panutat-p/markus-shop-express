db8b74812480abf1b0fe04826fe5a139e8ff3624
Panutat <49073281+panutat-p@users.noreply.github.com>
2022-03-08 23:22:54 +0700

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 export const MONGODB_USERNAME = "mongoAtlas";
-export const MONGODB_PASSWORD = "3FjlBdKq8L4rtGSN";
+export const MONGODB_PASSWORD = "";
 export const MONGODB_TEST = "test";
 export const MONGODB_MARKUS_SHOP = "markus-shop";
